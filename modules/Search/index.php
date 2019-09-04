a1cb47b2c57eb3d72a6d0e883ba4b42ba0447d0c
Ernest Buffington <ernest.buffington@gmail.com>
2019-09-03 22:48:54 -0400

    Update index.php

--- modules/Search/index.php
+++ modules/Search/index.php
@@ -62,23 +62,23 @@ switch($op) {
             $row = $db->sql_fetchrow($result);
             $topicimage = stripslashes($row['topicimage']);
             $topictext = stripslashes(check_html($row['topictext'], "nohtml"));
-            if (file_exists("themes/$ThemeSel/images/topics/$topicimage")) {
-                $topicimage = "themes/$ThemeSel/images/topics/$topicimage";
+            if (file_exists("themes/$ThemeSel/images/Blog_Topics/$topicimage")) {
+                $topicimage = "themes/$ThemeSel/images/Blog_Topics/$topicimage";
             } else {
                 $topicimage = $tipath.$topicimage;
             }
         } else {
             $topictext = _ALLTOPICS;
-            if (file_exists("themes/$ThemeSel/images/topics/AllTopics.gif")) {
-                $topicimage = "themes/$ThemeSel/images/topics/AllTopics.gif";
+            if (file_exists("themes/$ThemeSel/images/Blog_Topics/AllTopics.png")) {
+                $topicimage = "themes/$ThemeSel/images/Blog_Topics/AllTopics.png";
             } else {
-                $topicimage = $tipath.'AllTopics.gif';
+                $topicimage = $tipath.'AllTopics.png';
             }
         }
-        if (file_exists("themes/$ThemeSel/images/topics/AllTopics.gif")) {
-            $alltop = "themes/$ThemeSel/images/topics/AllTopics.gif";
+        if (file_exists("themes/$ThemeSel/images/Blog_Topics/AllTopics.png")) {
+            $alltop = "themes/$ThemeSel/images/Blog_Topics/AllTopics.png";
         } else {
-            $alltop = $tipath.'AllTopics.gif';
+            $alltop = $tipath.'AllTopics.png';
         }
         OpenTable();
         if ($type == 'users') {
